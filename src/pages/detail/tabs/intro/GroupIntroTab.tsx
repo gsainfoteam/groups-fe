001dda94915b864e19b9f367abeccea218b9e36f
SHIM-JINSEO <sjs011828@gmail.com>
2024-10-17 00:25:42 +0900

    fix: error about load notion record map

--- src/pages/detail/tabs/intro/GroupIntroTab.tsx
+++ src/pages/detail/tabs/intro/GroupIntroTab.tsx
@@ -8,21 +8,24 @@ import "./styles.css";
 
 import useSWR from "swr";
 import { getNotionPage } from "@/apis/notion";
+import { useParams } from "react-router-dom";
+import { getGroup } from "@/apis/group";
 //import NotionWrapper from "./NotionWrapper";
 
 interface GroupIntroTabProps {}
 
 const GroupIntroTab = ({}: GroupIntroTabProps) => {
-
-  const { data: recordMap } = useSWR(getNotionPage);
-
+  const {uuid} = useParams();
+  const {data : group } = useSWR(uuid,getGroup)
+  const { data: recordMap } = useSWR(group.notionPageId,getNotionPage);        
+  
   if (recordMap == null) {
-    return <div>Notion page not found</div>;
+    return <div>Notion page not found - Plese Check your notion ID</div>;
   }
-
+ 
   return (
     <div className={"mt-5"}>
-      <div>Notion page found</div>
+      <div>Notion page found - Todo: fix notionwrapper error</div>
       {/* <NotionWrapper recordMap={recordMap} /> */}
     </div>
   );
