2c541a7dc1224004f294810b3168fdcd9b4c344f
MiRoro2 <crowntheking@gm.gist.ac.kr>
2024-10-19 23:47:14 +0900

    feat: separate tab component from Tabs, to construct NavLinkTabs

--- src/pages/manage/components/Navigator.tsx
+++ src/pages/manage/components/Navigator.tsx
@@ -1,33 +1,36 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Navigator = () => {
   return (
     <div className="flex items-start h-11 md:h-[50px] w-full border-b-[3px] border-grey">
-      <NavLink 
-        to="/manage/groupinfo" 
+      <NavLink
+        to="/manage/groupinfo"
         className={({ isActive }) =>
           isActive
             ? "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-primary font-medium border-b-[3px] border-primary"
             : "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-grey font-medium"
-        }>
+        }
+      >
         기본 정보
       </NavLink>
-      <NavLink 
-        to="/manage/notionlink" 
+      <NavLink
+        to="/manage/notionlink"
         className={({ isActive }) =>
           isActive
             ? "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-primary font-medium border-b-[3px] border-primary"
             : "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-grey font-medium"
-        }>
+        }
+      >
         소개 페이지
       </NavLink>
-      <NavLink 
-        to="/manage/members" 
+      <NavLink
+        to="/manage/members"
         className={({ isActive }) =>
           isActive
             ? "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-primary font-medium border-b-[3px] border-primary"
             : "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-grey font-medium"
-        }>
+        }
+      >
         멤버
       </NavLink>
     </div>
