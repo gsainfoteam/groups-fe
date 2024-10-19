25e6814fdb080e10e263d784bef4eea80a3a06a7
MiRoro2 <crowntheking@gm.gist.ac.kr>
2024-10-20 00:08:14 +0900

    feat: replace Navigator in GroupManageLayout?

--- src/pages/manage/components/Navigator.tsx
+++ src/pages/manage/components/Navigator.tsx
@@ -1,38 +1,28 @@
-import { NavLink } from "react-router-dom";
+import { NavLinkTabs } from "@/components/tabs/Tabs";
+import Path from "@/types/paths";
 
 const Navigator = () => {
+  const tabs = [
+    {
+      key: "groupInfo",
+      label: "기본 정보",
+      link: Path.ManageGroupInfo,
+    },
+    {
+      key: "notionLink",
+      label: "소개 페이지",
+      link: Path.ManageNotionLink,
+    },
+    {
+      key: "members",
+      label: "멤버",
+      link: Path.ManageMembers,
+    },
+  ];
+
   return (
-    <div className="flex items-start h-11 md:h-[50px] w-full border-b-[3px] border-grey">
-      <NavLink
-        to="/manage/groupinfo"
-        className={({ isActive }) =>
-          isActive
-            ? "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-primary font-medium border-b-[3px] border-primary"
-            : "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-grey font-medium"
-        }
-      >
-        기본 정보
-      </NavLink>
-      <NavLink
-        to="/manage/notionlink"
-        className={({ isActive }) =>
-          isActive
-            ? "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-primary font-medium border-b-[3px] border-primary"
-            : "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-grey font-medium"
-        }
-      >
-        소개 페이지
-      </NavLink>
-      <NavLink
-        to="/manage/members"
-        className={({ isActive }) =>
-          isActive
-            ? "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-primary font-medium border-b-[3px] border-primary"
-            : "box-border h-11 md:h-[50px] flex p-2.5 justify-center items-center text-grey font-medium"
-        }
-      >
-        멤버
-      </NavLink>
+    <div className="w-full">
+      <NavLinkTabs tabs={tabs} />
     </div>
   );
 };
