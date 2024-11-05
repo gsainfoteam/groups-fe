9e6c1cfd0a15b8e77446dd70e8ac2964737bbfcd
SHIM-JINSEO <sjs011828@gmail.com>
2024-11-06 02:27:34 +0900

    feat: show group members with uuid (email and role will be added)

--- src/pages/detail/tabs/members/GroupMembersTab.tsx
+++ src/pages/detail/tabs/members/GroupMembersTab.tsx
@@ -6,25 +6,26 @@ import useSWR from "swr";
 
 const GroupMembersTab = () => {
   const {uuid} = useParams<{uuid:string}>()
-  console.log(uuid)
-  const {data, error, isLoading} = useSWR("UserInfo",getUserInfo)
-  const {data : members } = useSWR(`/group/${uuid}/member`, getGroupMember)
+  const {data : members, error, isLoading } = useSWR(`/group/${uuid}/member`, getGroupMember)
   console.log(members)
   if(error || isLoading){
     return <p>Loading members...</p>
   }
   
   return <div className="flex flex-col items-center">
+    {members.map(({name,uuid}:{name: string, uuid: string})=> 
     <div className={"flex items-center justify-between my-6 w-2/3 rounded-2xl bg-greyLight px-5 py-[15px] text-lg text-greyDark"}>
       <div>
-        <p>{data?.name}</p>
-        <p>{data?.email}</p>
+        <p>{name}</p>
+        <p>{uuid}</p> 
+        {/* uuid must be changed as email */}
       </div>
       <div>
         Admin
+        {/* This part must be changed as role */}
       </div>
-    </div>
-  </div>;
+    </div>)}
+  </div>
 };
 
 export default GroupMembersTab;
