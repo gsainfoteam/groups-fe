ab98e7f897e1c61587fd671136e8a15e6bfebe50
Do-hyun Ko <dohyun682@gmail.com>
2024-11-06 20:42:39 +0900

    :bug: Add error handling logic for dataUrlToFile func

--- src/utils/dataURLtoFile/index.ts
+++ src/utils/dataURLtoFile/index.ts
@@ -2,7 +2,26 @@ export async function dataUrlToFile(
   dataUrl: string,
   fileName: string,
 ): Promise<File> {
-  const res: Response = await fetch(dataUrl);
-  const blob: Blob = await res.blob();
-  return new File([blob], fileName, { type: "image/png" });
+  if (!dataUrl?.trim()) {
+    throw new Error("Invalid dataUrl.");
+  }
+  if (!fileName?.trim()) {
+    throw new Error("Invalid fileName.");
+  }
+  try {
+    const res: Response = await fetch(dataUrl);
+    if (!res.ok) {
+      throw new Error("Failed to fetch the image.");
+    }
+    const blob: Blob = await res.blob();
+    return new File([blob], fileName, { type: "image/png" });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(
+        `An error occurred during file conversion: ${error.message}`,
+      );
+    } else {
+      throw new Error("An unknown error occurred during file conversion.");
+    }
+  }
 }
