6f71f10acb5ee96fbbc0d397db9a2f6231f7ac5c
MiRoro2 <crowntheking@gm.gist.ac.kr>
2024-11-13 12:20:47 +0900

    refactor: remove redundant props in navbar

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,11 @@
 import Navbar from "@/components/navbar/Navbar";
-import Path from "@/types/paths";
 
 import { Outlet } from "react-router-dom";
 
-interface LayoutProps {
-  to?: string;
-}
-
-const Layout = ({ to = Path.Home }: LayoutProps) => {
+const Layout = () => {
   return (
     <>
-      <Navbar to={to} />
+      <Navbar />
 
       <Outlet />
     </>
