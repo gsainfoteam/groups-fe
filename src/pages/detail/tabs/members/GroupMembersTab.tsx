37aee9105bc7914d8c1052fd7aff2e4d14cc28a0
crown-3 <crowntheking@gm.gist.ac.kr>
2025-01-27 11:59:20 +0900

    style: apply better UX for groupdetail member tab

--- src/pages/detail/tabs/members/GroupMembersTab.tsx
+++ src/pages/detail/tabs/members/GroupMembersTab.tsx
@@ -5,6 +5,7 @@ import { getGroupMember } from "@/apis/group";
 import { useParams } from "react-router-dom";
 import { ExpandedGroupInfo } from "@/types/interfaces";
 import UserCircle from "@/assets/icons/user-circle.svg?react";
+import Loading from "@/components/loading/Loading";
 interface GroupMemberProps {
   group: ExpandedGroupInfo;
 }
@@ -18,14 +19,14 @@ const GroupMembersTab = ({ group }: GroupMemberProps) => {
     isLoading,
   } = useSWR(`/group/${uuid}/member`, getGroupMember);
   if (isLoading) {
-    return <Card className={"mt-6"}>Loading members...</Card>;
+    return <Loading />;
   }
   if (error) {
     return <Card className={"mt-6"}>Failed to load members.</Card>;
   }
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex">
+    <div className="flex flex-col items-center py-10">
+      <div className="flex gap-2">
         <UserCircle />
         <div className={"text-[20px] text-greyDark"}>{group.memberCount}명</div>
       </div>
@@ -37,11 +38,13 @@ const GroupMembersTab = ({ group }: GroupMemberProps) => {
         ) => (
           <Card
             key={email ?? idx}
-            className={"flex items-center justify-between mt-6 w-2/3"}
+            className={
+              "flex items-center justify-between mt-6 w-full max-w-[600px] gap-4"
+            }
           >
             <div>
-              <p>{name}</p>
-              <p>{email}</p>
+              <p className="font-semibold text-dark">{name}</p>
+              <p className="text-dark">{email}</p>
             </div>
             <div>{role}</div>
           </Card>
