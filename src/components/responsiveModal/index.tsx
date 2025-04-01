0dbd378c50c56dcc74d656b573f4c76b2d6db2e5
crown-3 <crowntheking@gm.gist.ac.kr>
2025-04-01 22:42:04 +0900

    feat: add respontive modal

--- src/components/responsiveModal/index.tsx
+++ src/components/responsiveModal/index.tsx
@@ -1,5 +1,37 @@
-const ResponsiveModal = () => {
-  return <div>ResponsiveModal</div>;
+import Modal, { ModalProps } from "../modal";
+
+import { BottomSheet, BottomSheetProps } from "react-spring-bottom-sheet";
+
+interface ResponsiveModalProps {
+  commonProps: {
+    isOpen: boolean;
+    onClose: () => void;
+  };
+  modalProps: Omit<ModalProps, "onClose">;
+  bottomSheetProps: Omit<BottomSheetProps, "open" | "onDismiss">;
+}
+
+const ResponsiveModal = (props: ResponsiveModalProps) => {
+  const { commonProps, modalProps, bottomSheetProps } = props;
+
+  return (
+    <>
+      <div className="block md:hidden">
+        <Modal {...modalProps} onClose={commonProps.onClose}>
+          {modalProps.children}
+        </Modal>
+      </div>
+      <div className="hidden md:block">
+        <BottomSheet
+          open={commonProps.isOpen}
+          onDismiss={commonProps.onClose}
+          {...bottomSheetProps}
+        >
+          {bottomSheetProps.children}
+        </BottomSheet>
+      </div>
+    </>
+  );
 };
 
 export default ResponsiveModal;
