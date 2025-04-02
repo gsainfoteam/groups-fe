b60d7462cebbf52a71d082923fd72d1126220ba3
crown-3 <crowntheking@gm.gist.ac.kr>
2025-04-02 18:30:46 +0900

    feat: add localstorage cleanup in group creation sequence

--- src/pages/createGroup/layout/CreateGroupLayout.tsx
+++ src/pages/createGroup/layout/CreateGroupLayout.tsx
@@ -1,42 +1,59 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { GroupCreationProvider } from "../context/GroupCreationContext";
+import {
+  GroupCreationProvider,
+  useGroupCreation,
+} from "../context/GroupCreationContext";
 import StatusBar from "../components/StatusBar";
 import stepGetter from "../components/stepGetter";
+import { useEffect } from "react";
 
-const CreateGroupLayout = () => {
+const CreateGroupLayoutContent = () => {
   const { t } = useTranslation();
   const pathname = useLocation().pathname;
   const stepParam = pathname.split("/").pop();
   const step = stepGetter(stepParam);
+  const { resetState } = useGroupCreation();
+
+  useEffect(() => {
+    return () => {
+      resetState();
+    };
+  }, []);
 
   return (
+    <main className="flex flex-col items-center py-10">
+      <div className="content flex max-w-[800px] flex-col">
+        <h1 className="mb-[25px] text-4xl font-bold">
+          {t("createGroup.createGroup")}
+        </h1>
+
+        <div className="flex items-center gap-[10px]">
+          <div className="text-lg font-bold text-primary">
+            {step.stepTranslation}
+          </div>
+          <div className="h-[1px] w-[40px] bg-greyDark" />
+          <div className="text-lg font-medium text-greyDark">
+            {step.stepNameTranslation}
+          </div>
+        </div>
+
+        <div className="mt-[14px] w-full">
+          <StatusBar maxStep={4} currentStep={step.step} />
+        </div>
+      </div>
+
+      <div className="content flex max-w-[800px] flex-col items-center">
+        <Outlet />
+      </div>
+    </main>
+  );
+};
+
+const CreateGroupLayout = () => {
+  return (
     <GroupCreationProvider>
-      <main className="flex flex-col items-center py-10">
-        <div className="content flex max-w-[800px] flex-col">
-          <h1 className="mb-[25px] text-4xl font-bold">
-            {t("createGroup.createGroup")}
-          </h1>
-
-          <div className="flex items-center gap-[10px]">
-            <div className="text-lg font-bold text-primary">
-              {step.stepTranslation}
-            </div>
-            <div className="h-[1px] w-[40px] bg-greyDark" />
-            <div className="text-lg font-medium text-greyDark">
-              {step.stepNameTranslation}
-            </div>
-          </div>
-
-          <div className="mt-[14px] w-full">
-            <StatusBar maxStep={4} currentStep={step.step} />
-          </div>
-        </div>
-
-        <div className="content flex max-w-[800px] flex-col items-center">
-          <Outlet />
-        </div>
-      </main>
+      <CreateGroupLayoutContent />
     </GroupCreationProvider>
   );
 };
