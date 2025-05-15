1b2361d0138083a97ec77af67d3f92f8e407c0c6
SHIM-JINSEO <sjs011828@gmail.com>
2025-05-15 18:04:24 +0900

    fix: fix groups client ids in .env and add prompt

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -3,15 +3,15 @@ import api from "./interceptor";
 import apiKeys from "@/types/api-keys";
 import LocalStorageKeys from "@/types/localstorage";
 import * as client from "openid-client";
-const IDP_BASE_URL = import.meta.env.VITE_IDP_BASE_URL;
+
+const IDP_API_URL = import.meta.env.VITE_IPD_API_URL;
 const CLIENT_ID = import.meta.env.VITE_IDP_CLIENT_ID;
 const REDIRECT_URI = import.meta.env.VITE_IDP_REDIRECT_URI;
 
 export const generateOAuthLoginURL = async () => {
   try {
-    let server: URL = new URL(
-      "https://api.idp.gistory.me/.well-known/openid-configuration",
-    );
+    console.log(`${IDP_API_URL}.well-known/openid-configuration`);
+    let server: URL = new URL(`${IDP_API_URL}.well-known/openid-configuration`);
     let config = await client.discovery(server, CLIENT_ID);
 
     const nonce = client.randomNonce();
@@ -28,21 +28,11 @@ export const generateOAuthLoginURL = async () => {
       code_challenge_method: "S256",
       redirect_uri: REDIRECT_URI,
       scope: "openid profile email",
+      prompt: "login",
       state: state,
       nonce: nonce,
     };
-    // const params = new URLSearchParams({
-    //   response_type: "code",
-    //   client_id: CLIENT_ID,
-    //   redirect_uri: REDIRECT_URI,
-    //   scope: "openid profile email",
-    //   prompt: "login",
-    //   state,
-    // });
-
-    // const authUrl = `${IDP_BASE_URL}authorize?${params.toString()}`;
     const authUrl = client.buildAuthorizationUrl(config, parameters);
-    console.log("authUrl", authUrl);
     return authUrl.toString();
   } catch (error) {
     console.error("Error in generateOAuthLoginURL:", error);
